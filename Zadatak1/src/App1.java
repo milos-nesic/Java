062882e81295e8fa959b5a842251543dd295b3ac
milos-nesic <milos.nesic@uniqa-see.digital>
2024-10-14 14:40:04 +0200

    test

--- Zadatak1/src/App1.java
+++ Zadatak1/src/App1.java
@@ -1,6 +1,6 @@
 
 
-public class App {
+public class App1 {
     public static void main(String[] args) {
         // Initializing variables here:
         int x = 25, y = 35, z = 45;
@@ -8,5 +8,7 @@ public class App {
         System.out.println("Your current position is: ");
         System.out.printf("x:%d,y:%d,z:%d\n",
                 currentPosition[0], currentPosition[1], currentPosition[2]);
+
+    
     }
 }
