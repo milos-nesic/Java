4318c0d0a7806ae7c8b4cce08433f00c90154cce
milos-nesic <milos.nesic@uniqa-see.digital>
2024-10-15 12:31:27 +0200

    Vezba5

---
+++ AutomatskoTestiranje/src/Vezba5.java
@@ -0,0 +1,16 @@
+
+public class Vezba5 {
+
+    
+    /** 
+     * Metoda koja sabira dva broja
+     * @param args
+     */
+    public static void main(String[] args) {
+        int operand1 = 2;
+        int operand2 = 3;
+        int sum = operand1 + operand2;
+        System.out.println(sum);
+    }
+
+}
