244b901a65392e9fcd219aa9533e72e6b9dbd3a1
milos-nesic <milos.nesic@uniqa-see.digital>
2024-10-15 12:22:16 +0200

    Vezba6

---
+++ AutomatskoTestiranje/src/Vezba6.java
@@ -0,0 +1,14 @@
+
+public class Vezba6 {
+
+    public static void main(String[] args) {
+        int a = 2, b = 3;
+        // System.out.println("First operand: ");
+        // System.out.println(a);
+        // System.out.println("Second operand: ");
+        // System.out.println(b);
+        // System.out.println("Sum: ");
+        System.out.println(a + b);
+    }
+
+}
