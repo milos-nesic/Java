a975e95433a2b84d12dd058b9607a231bf210b8c
milos-nesic <milos.nesic@uniqa-see.digital>
2024-10-15 11:36:19 +0200

    Brisanje

--- AutomatskoTestiranje/src/Zadatak2.java
+++ AutomatskoTestiranje/src/Zadatak2.java
@@ -1,6 +1,6 @@
 
 
-public class App1 {
+public class Zadatak2 {
     public static void main(String[] args) {
         // Initializing variables here:
         int x = 25, y = 35, z = 45;
@@ -9,6 +9,9 @@ public class App1 {
         System.out.printf("x:%d,y:%d,z:%d\n",
                 currentPosition[0], currentPosition[1], currentPosition[2]);
 
+
+
     
+
     }
 }
