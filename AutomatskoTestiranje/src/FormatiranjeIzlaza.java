0c947d509a696d4a9482cbe6a27232cf27854169
milos-nesic <milos.nesic@uniqa-see.digital>
2024-10-15 13:30:11 +0200

    Formatiranje Izlaza

---
+++ AutomatskoTestiranje/src/FormatiranjeIzlaza.java
@@ -0,0 +1,9 @@
+
+public class FormatiranjeIzlaza {
+
+    public static void main(String[] args) {
+        double x = 113. / 112;
+        System.out.println(x);
+        System.out.printf("%.4f", x);
+    }
+}
